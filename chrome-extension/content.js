4408a99b35095207b401bbb158a096093f35aac7
Sam <sroose@gmail.com>
2020-09-16 10:15:34 +0200

    Removed unneeded file

--- chrome-extension/content.js
+++
@@ -1,8 +0,0 @@
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    alert('updated from contentscript');
-   if(tab.url.includes('google.com')) {
-      chrome.browserAction.setIcon({path:"iconGreen.png"});
-    }
- });
-
-
