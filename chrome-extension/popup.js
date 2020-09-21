72e37a43e5e4e42dc0afcb4c7cd62215e601f68f
Sam <sroose@gmail.com>
2020-09-21 20:24:02 +0200

    Added a lot of sites; take into account country-specific links

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -10,7 +10,11 @@ document.addEventListener('DOMContentLoaded', function() {
   current_domain = current_url.match(/^[\w-]+:\/{2,}\[?([\w\.:-]+)\]?(?::[0-9]*)?/)[1];
   
   if(is_partner) {
-    document.getElementById('isPartner').innerHTML  = "This site gives satsback! <br><span style='font-size: 1.4em;'><a href='https://stekking.com/satsback/" + partner_id + "' target='_blank'>Open via Stekking website</a></span>";
+    if(partner_id === 'country_specific') {
+      document.getElementById('isPartner').innerHTML  = "This site gives satsback! <br>This site has country specific links.<br><span style='font-size: 1.4em;'><a href='https://stekking.com/shops' target='_blank'>Open the Stekking website</a> and search the site for your country!</span>";
+    } else {
+      document.getElementById('isPartner').innerHTML  = "This site gives satsback! <br><span style='font-size: 1.4em;'><a href='https://stekking.com/satsback/" + partner_id + "' target='_blank'>Open via Stekking website</a></span>";
+    }
     document.getElementById('wrongQuestion').innerHTML  = "Is this site incorrectly flagged as stekking partner? Let us know!";
     document.getElementById('report').innerHTML  = "Report site as false positive";
     document.getElementById('report').href = "https://github.com/sroose/stekking/issues/new?labels[]=report&labels[]=[Type]%20Bug&title=" + encodeURI(current_domain) + "&body=The%20site%20" + encodeURI(current_url) + "%20is%20not%20a%20valid%20Stekking%20site.";
